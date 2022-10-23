61c1f385fa0304f6814bd5e7a84decb14134ddf5
JocheRey <hugojosevalladaresreyes27@gmail.com>
2022-10-22 22:48:50 -0600

    Configuraciones de entorno

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('inicio');
+    return view('welcome');
 });
-
